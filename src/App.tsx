0fae850e474391d5740cd45b347d02d200df3438
jwo0o0 <jwo0o0358@gmail.com>
2023-07-07 04:20:09 +0900

    log2

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ import { NotFoundPage } from '@pages/NotFound';
 import { Layout } from '@layout/index';
 
 const staticServerUri = import.meta.env.VITE_REACT_APP_PATH || '';
-console.log('staticServerUri:', staticServerUri);
 
 function App() {
+  console.log('staticServerUri:', staticServerUri);
   return (
     <BrowserRouter>
       <GlobalStyle />
