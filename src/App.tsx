bc5d7673722f302db1952436e92ffd93c873251a
jwo0o0 <jwo0o0358@gmail.com>
2023-07-06 12:59:02 +0900

    chore: assets 절대경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './styles/global-style';
+import GlobalFonts from '@assets/fonts';
 
 import { MainPage } from '@pages/Main';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <GlobalFonts />
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
--- src/assets/fonts/index.ts
+++ src/assets/fonts/index.ts
@@ -4,14 +4,14 @@ import NanumSquareNeoRegular from './NanumSquareNeoTTF-bRg.woff2';
 
 export default createGlobalStyle`
 @font-face {
-    font-family: "PretendardRegular";
+    font-family: "Pretendard";
     src: local("PretendardRegular"),
     url(${PretendardRegular}) format('woff2');
     font-style: normal;
 }
 @font-face {
-    font-family: "NanumSquareNeoRegular";
-    src: local("NanumSquareNeoRegular"),
+    font-family: "NanumSquare";
+    src: local("NanumSquare"),
     url(${NanumSquareNeoRegular}) format('woff2');
     font-style: normal;
 }
