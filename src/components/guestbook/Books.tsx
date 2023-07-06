459601914c4f38370aabd8a7acad9c15e29f6eb4
jwo0o0 <jwo0o0358@gmail.com>
2023-07-06 16:49:52 +0900

    feat: 방명록 레이아웃

--- src/components/guestbook/Books.tsx
+++ src/components/guestbook/Books.tsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
 import { WriteBtn } from '@components/guestbook/WriteBtn';
+import { Post } from '@components/guestbook/Post';
 
 export const Books = () => {
   return (
     <BooksContainer>
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
       <WriteBtn />
     </BooksContainer>
   );
@@ -17,16 +18,16 @@ export const Books = () => {
 
 const BooksContainer = styled.div`
   margin-top: 155px;
+
   position: relative;
   height: calc(100% - 160px);
+
   overflow-y: scroll;
   &::-webkit-scrollbar {
     display: none;
   }
-`;
 
-const Box = styled.div`
-  background-color: aliceblue;
-  height: 200px;
-  margin: 10px 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
 `;
