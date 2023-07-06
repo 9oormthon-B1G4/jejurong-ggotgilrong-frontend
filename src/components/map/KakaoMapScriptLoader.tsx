29cf8aee7b49fa792ec044437462f0dd2b85ade4
bbung <tjd123123@naver.com>
2023-07-06 20:42:50 +0900

    feat: kakao Map API 설정

---
+++ src/components/map/KakaoMapScriptLoader.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+const KAKAO_MAP_SCRIPT_ID = 'kakaoMapScript';
+
+interface KakaoMapScriptLoaderProps {
+  children: React.ReactNode;
+}
+
+const KakaoMapScriptLoader = ({ children }: KakaoMapScriptLoaderProps) => {
+  const [isSetMapScript, setIsMapScript] = useState<boolean>(false);
+
+  useEffect(() => {
+    const mapScript = document.getElementById(KAKAO_MAP_SCRIPT_ID);
+
+    if (mapScript && !window.kakao) return;
+
+    const script = document.createElement('script');
+    script.id = KAKAO_MAP_SCRIPT_ID;
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${
+      import.meta.env.VITE_KAKAO_API_KEY
+    }&autoload=false&libraries=clusterer`;
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        setIsMapScript(true);
+      });
+    };
+
+    script.onerror = () => {
+      setIsMapScript(false);
+    };
+    document.getElementById('root')?.appendChild(script);
+  }, []);
+
+  if (!isSetMapScript) {
+    return <></>;
+  }
+
+  return <>{children}</>;
+};
+
+export default KakaoMapScriptLoader;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import KakaoMapScriptLoader from '@components/map/KakaoMapScriptLoader.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <KakaoMapScriptLoader>
+      <App />
+    </KakaoMapScriptLoader>
   </React.StrictMode>,
 );
