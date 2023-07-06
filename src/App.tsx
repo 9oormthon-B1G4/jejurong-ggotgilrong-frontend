3a94110635bd2604598f4ffcc91cd33008a3ef3e
bbung <tjd123123@naver.com>
2023-07-06 11:55:29 +0900

    refactor: layout 구조변경, mobile 환경 높이 대응

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import { MainPage } from '@pages/Main';
 import { GuestBookPage } from '@pages/Guestbook';
 import { LandingPage } from '@pages/Landing';
 import { NotFoundPage } from '@pages/NotFound';
+import { Layout } from '@layout/index';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/main/:id/book" element={<GuestBookPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/main/:id/book" element={<GuestBookPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@styles/theme';
 
@@ -8,6 +8,19 @@ interface LayoutProps {
 }
 
 export const Layout = ({ children }: LayoutProps) => {
+  /**
+   * mobile height size 설정
+   */
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    window.addEventListener('resize', () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }, []);
+
   return (
     <LayoutContainer>
       <ThemeProvider theme={theme}>{children}</ThemeProvider>
