6c9d932b7a7f647b30bdb5f143ead632236ab5df
bbung <tjd123123@naver.com>
2023-07-07 07:19:24 +0900

    chore: baseURL 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/k76a5a404cfe0a',
+  base: '/',
   plugins: [tsconfigPaths()],
   server: {
     port: 3000,
