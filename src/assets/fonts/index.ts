baf2be491564677e629ab6513928ba157447471e
jwo0o0 <jwo0o0358@gmail.com>
2023-07-07 09:16:20 +0900

    style: 폰트 추가

--- src/assets/fonts/index.ts
+++ src/assets/fonts/index.ts
@@ -1,5 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 import PretendardRegular from './Pretendard-Regular.woff2';
+import PretendardBold from './Pretendard-Bold.woff2';
+import PretendardSemiBold from './Pretendard-SemiBold.woff2';
+import PretendardMedium from './Pretendard-Medium.woff2';
+import PretendardExtraBold from './Pretendard-ExtraBold.woff2';
 import NanumSquareNeoRegular from './NanumSquareNeoTTF-bRg.woff2';
 import NanumSquareRound from './NanumSquareRoundR.ttf';
 
@@ -8,6 +12,30 @@ export default createGlobalStyle`
     font-family: "Pretendard";
     src: local("PretendardRegular"),
     url(${PretendardRegular}) format('woff2');
+    font-style: normal;
+}
+@font-face {
+    font-family: "Pretendard";
+    src: local("PretendardBold"),
+    url(${PretendardBold}) format('woff2');
+    font-style: normal;
+}
+@font-face {
+    font-family: "PretendardSemiBold";
+    src: local("PretendardSemiBold"),
+    url(${PretendardSemiBold}) format('woff2');
+    font-style: normal;
+}
+@font-face {
+    font-family: "PretendardMedium";
+    src: local("PretendardMedium"),
+    url(${PretendardMedium}) format('woff2');
+    font-style: normal;
+}
+@font-face {
+    font-family: "PretendardExtraBold";
+    src: local("PretendardExtraBold"),
+    url(${PretendardExtraBold}) format('woff2');
     font-style: normal;
 }
 @font-face {
